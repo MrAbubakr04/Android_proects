155af1c5af7a43e7b0b1b2a28c551923096d1f3f
Abubark_04 <clasher04abubakr@gmail.com>
2024-04-12 23:32:57 +0500

    1_change

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import secondActivity.seconddActivity
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val button:Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this,seconddActivity::class.java)
+            var m1:Boolean = true
+            var m2:Boolean = true
+            val editText1 = findViewById<EditText>(R.id.editext1)
+            val editText2 = findViewById<EditText>(R.id.editext2)
+            editText1.doAfterTextChanged { editable ->
+                m1 = false
+            }
+            editText2.doAfterTextChanged { editable ->
+                m2 = false
+            }
+            val text2 = editText2.text?.toString()
+            val text1 = editText1.text?.toString()
+            if (m1 || m2 ){
+                val text = "Введите пароль"
+                val duration = Toast.LENGTH_SHORT // Или Toast.LENGTH_LONG для длительного отображения
+
+                val toast = Toast.makeText(applicationContext, text, duration)
+                toast.show()
+            } else if(text1 == text2){
+                startActivity(intent)
+            } else {
+                val text = "Коды не совпадают"
+                val duration = Toast.LENGTH_SHORT // Или Toast.LENGTH_LONG для длительного отображения
+
+                val toast = Toast.makeText(applicationContext, text, duration)
+                toast.show()
+
+            }
+
+        }
+
+    }
+}
+
