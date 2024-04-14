7ed7f6eeee81ca4ba77bb4cd43d0a4c61ffb3e3d
MrAbubakr04 <MrAbubakr04>
2024-04-14 23:45:06 +0500

    proect_2

--- app/src/main/java/secondActivity/NewsActualy.kt
+++ app/src/main/java/secondActivity/NewsActualy.kt
@@ -1,4 +1,12 @@
 package secondActivity
 
-class NewsActualy {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class NewsActualy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
 }
