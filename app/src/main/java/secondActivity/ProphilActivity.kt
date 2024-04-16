92f7be6a0504af1706f4b76973e2bbbc6dbf2c5e
MrAbubakr04 <MrAbubakr04>
2024-04-16 16:51:59 +0500

    proect_2

---
+++ app/src/main/java/secondActivity/ProphilActivity.kt
@@ -0,0 +1,12 @@
+package secondActivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class ProphilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prophil)
+    }
+}
