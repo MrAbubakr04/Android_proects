ee6bb9b89a89b7a321efde994c9fac0c07eb4e8e
MrAbubakr04 <MrAbubakr04>
2024-04-18 22:40:25 +0500

    proect_4

---
+++ app/src/main/java/secondActivity/Perevods.kt
@@ -0,0 +1,12 @@
+package secondActivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class Perevods : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perevods_first)
+    }
+}
