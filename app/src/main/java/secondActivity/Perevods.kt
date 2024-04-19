0be422ec3be892e50783fd11c24e5fc4700a9a5b
MrAbubakr04 <MrAbubakr04>
2024-04-19 22:38:33 +0500

    Perevods

--- app/src/main/java/secondActivity/Perevods.kt
+++ app/src/main/java/secondActivity/Perevods.kt
@@ -1,12 +1,22 @@
 package secondActivity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 
 class Perevods : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_perevods_first)
+        val button:com.google.android.material.button.MaterialButton = findViewById(R.id.humo_perevod_botton1)
+        button.setOnClickListener {
+            val intent = Intent(this,SecondPerevodsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/secondActivity/SecondPerevodsActivity.kt
@@ -0,0 +1,12 @@
+package secondActivity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+class SecondPerevodsActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perevods_second)
+    }
+}
