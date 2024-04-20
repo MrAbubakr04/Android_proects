d4f228997fd73641076d28a7bed0c5a66e8c7bc5
MrAbubakr04 <MrAbubakr04>
2024-04-20 09:26:56 +0500

    Perevods

--- app/src/main/java/secondActivity/SecondPerevodsActivity.kt
+++ app/src/main/java/secondActivity/SecondPerevodsActivity.kt
@@ -2,11 +2,22 @@ package secondActivity
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
+import androidx.appcompat.widget.Toolbar
 class SecondPerevodsActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_perevods_second)
+        val toolbar:Toolbar = findViewById(R.id.humo_perevod_toolbar1)
+        setSupportActionBar(toolbar)// Показать кнопку возврата назад
+        supportActionBar?.setDisplayShowTitleEnabled(false)// Удалить заголовок
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed() // При клике на кнопку возврата назад будет вызываться метод onBackPressed()
+        }
     }
 }
